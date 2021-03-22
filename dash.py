d8f05fcf69192d2d16da31d52bb9a4f21b2d3d44
Chloe CARAYON <chloe.carayon@efrei.net>
2021-03-22 18:18:44 +0100

    modification + ajout graphs

--- dash.py
+++ dash.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 import dash
 import pandas as pd
@@ -8,41 +7,44 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
+#import plotly_wordcloud
+from plotly.offline import iplot, init_notebook_mode
+
 
 app = dash.Dash()
 
-df = pd.read_csv("ufo.csv")
-df.datetime = df.datetime.map(lambda x:  int(x.split('-')[2].split(" ")[0]))
+df = pd.read_csv("data/ufo.csv")
 
 colors = {
     'background': '#E5EFF4',
     'text': '#516D90',
 }
 
-#fig0 = ff.create_table(df[:20])
+df['datetime_map'] = df.datetime.map(lambda x:  int(x.split('-')[2].split(" ")[0]))
 
-#fig1 = px.histogram(df, x="total_bill", color="sex")
+df["duration_cut"] = pd.cut(x=df['duration'], bins=[0, 60, 600, 900, 1800, 3600, 7200, 10800])
 
-#fig2 = px.bar(df, x="sex", y="total_bill", color="smoker", barmode="group")
+#print(df.groupby('duration').count())
 
+# example show form specific !
+df = df[df.form == 'cone']
 
-#fig4 = px.scatter(df, x="total_bill", y="tip", color="sex", facet_col="smoker")
-#fig5 = px.box(df, y="tip", x="smoker", color="sex")
-#fig6 = px.box(df, x="day", y="total_bill", color="smoker")
-#fig7 = px.pie(df, values='tip', names='day')
-#fig8 = px.scatter(df, x="total_bill", y="tip", color="day", facet_col="time")
+df['datetime'] = pd.to_datetime(df['datetime'])
 
+df['year'] = df.datetime.dt.year
+df['month'] = df.datetime.dt.month
+df['day'] = df.datetime.dt.day
 
-#df = df.query("country =='us'")
-df = df[df.city == 'san marcos']
+year_comments = df.groupby('year')['comments'].count()
 
-fig = go.Figure(go.Densitymapbox(lat=df.latitude, lon=df.longitude, z=df.datetime, radius=10))
+form_count = df.form.value_counts().index
+
+fig = go.Figure(go.Densitymapbox(lat=df.latitude, lon=df.longitude, z=df.datetime_map, radius=10))
 fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
 fig.update_layout(margin={"r":10,"t":10,"l":10,"b":10})
-#fig.show()
-
-#fig1 = px.choropleth(lat=df.latitude, lon=df.longitude )
-
+fig1 = px.histogram(df, x="year", color="form")
+fig2 = px.histogram(df, x="duration", color="form")
+#fig3 = px.pie(df, values='form_count', names='form_count.index')
 
 app.layout = html.Div([
     html.H3(children='Chloé CARAYON - Victor TAILLIEU', style={
@@ -51,19 +53,24 @@ app.layout = html.Div([
     }),
 
     dcc.Graph(
-        id='dataset',
+        id='Map',
         figure=fig
     ),
+
+    dcc.Graph(
+        id='Repartition',
+        figure=fig1
+    ),
+
+    dcc.Graph(
+        id='Repartition2',
+        figure=fig2
+    ),
+
 
 
 ])
 
-
-# legends
-#
-
-
-
-
 if __name__ == '__main__':
    app.run_server(debug=True)
+
