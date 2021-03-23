924ad9e6074d0858f24baec0a8d8246b194ec917
Chloe CARAYON <chloe.carayon@efrei.net>
2021-03-23 19:34:57 +0100

    add plot : subplots dates

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
-
+from plotly.subplots import make_subplots
 import pandas as pd
 
 ## Load dataset
@@ -24,21 +24,19 @@ df['year'] = df.datetime.dt.year
 df['month'] = df.datetime.dt.month
 df['day'] = df.datetime.dt.day
 
+
+
 colors = {
     'background': '#E5EFF4',
     'text': '#516D90',
 }
 
-# example show form specific !
-# df = df[df.form == 'cone']
-
-
 year_comments = df.groupby('year')['comments'].count()
 
 form_count = df.form.value_counts().index
 
-type_labels = ["Count", "Duration", "Form"]
-type_values = ["count", "duration", "form"]
+type_labels = ["Count", "Duration", "Form", "Date"]
+type_values = ["count", "duration", "form", "date"]
 
 
 ## Application layout
@@ -95,8 +93,18 @@ def update_graph(graph_type, select_values):
         dff = dff[dff.country != "gb"]
 
     duration_comments = dff.groupby('duration_cut')['comments'].count()
+    new_index = ['0-1min', '1-10mins', '10-15mins', '15-30mins',
+                '30mins-1h','1h-2h','2h-3h']
+    duration_comments.index = new_index
     form_count = dff.groupby('form')['comments'].count()
     form_count = form_count[form_count >= 10]
+
+    month_comments = dff.groupby('month')['comments'].count()
+    new_index = ['jan', 'feb', 'mar', 'apr',
+                'may','jun','jul','aug','sep','oct','nov','dec']
+    month_comments.index = new_index
+    day_comments = dff.groupby('day')['comments'].count()
+
 
     fig1 = go.Figure(go.Densitymapbox(lat=dff.latitude, lon=dff.longitude, z=dff.datetime_map, radius=10))
     fig1.update_layout(mapbox_style="stamen-terrain",
@@ -106,10 +114,21 @@ def update_graph(graph_type, select_values):
     if graph_type == "count":
         fig2 = px.histogram(dff, x="year", color="form")
     elif graph_type == "duration":
-        fig2 = px.bar(x=duration_comments, color=duration_comments.index)
-    else:
+        fig2 = px.bar(x=duration_comments, color=duration_comments.index, title="Time duration of ufo")
+    elif graph_type == "form":
         fig2 = px.pie(values=form_count, names=form_count.index)
+    else:
+        fig2 = make_subplots(rows=2, cols=1)
 
+        fig2.add_trace(
+            go.Bar(x=day_comments.index, y=day_comments, name="days"),
+            row=1, col=1
+        )
+
+        fig2.add_trace(
+            go.Bar(x=month_comments.index, y=month_comments, name="months"),
+            row=2, col=1
+        )
     return [fig1, fig2]
 
 
